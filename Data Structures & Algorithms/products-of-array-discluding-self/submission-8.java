ea4b238264bc77d55647bc7dc37830a8ec036473
Tom Thomas <txt450@student.bham.ac.uk>
2026-08-18 23:16:20 +0100

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        //calc prefix product of each element 
+        //then multiply each prefix by its suffix which you can calc by just
+        //iterating down your nums array. 
+        int[] prefix = new int[nums.length]; 
+        int pref = 1; 
+        prefix[0] = pref; 
+        for(int i = 1; i < nums.length; i++)
+        {
+            pref = pref * nums[i-1];
+            prefix[i] = pref; 
+        }
+        int suffix = 1; 
+        for(int i = nums.length-1; i >= 0; i--)
+        {
+            prefix[i] = prefix[i]*suffix; 
+            suffix = suffix * nums[i]; 
+        }
+        return prefix; 
+    }
+}  
