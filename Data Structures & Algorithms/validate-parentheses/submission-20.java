41334482cbd45418cfa476aa0ba8e7d70de31979
Tom Thomas <txt450@student.bham.ac.uk>
2026-09-13 12:54:34 +0100

    Add: validate-parentheses - submission-20

---
+++ Data Structures & Algorithms/validate-parentheses/submission-20.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> st = new ArrayDeque<>();
+        int n = s.length(); 
+        if((n%2) != 0) return false;
+        HashMap<Character, Character> mp = new HashMap<>(); 
+        mp.put('}', '{');
+        mp.put(']', '['); 
+        mp.put(')', '(');
+        for (int i = 0; i < n; i++) {
+            if(mp.containsValue(s.charAt(i))) {
+                st.push(s.charAt(i)); 
+            }
+            else if(st.isEmpty()) {
+            return false;
+            } 
+            else if(!st.isEmpty() && st.peek() == mp.get(s.charAt(i)))
+            {
+                st.pop();
+            } else if (!st.isEmpty() && st.peek()!= mp.get(s.charAt(i))
+            ) {
+                return false; 
+            }
+        }
+        if(!st.isEmpty()) return false; 
+        return true; 
+    }
+}
