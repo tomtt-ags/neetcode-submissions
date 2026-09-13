dacb5cace206d42bd401e881451f5d27424c0268
Tom Thomas <txt450@student.bham.ac.uk>
2026-09-13 17:28:29 +0100

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<String> st = new Stack<>(); 
+        for(int i = 0; i < tokens.length; i++) {
+            st.push(tokens[i]); 
+        }
+        return recurse(st);  
+    }
+    public int recurse (Stack<String> st) {
+        if(st.size() == 1) {
+            String s = st.pop(); 
+            return Integer.parseInt(s); 
+        }
+        String n = st.pop(); 
+        if(n.equals("-")) {
+            int b = recurse(st);
+            int a = recurse(st);
+            return a - b; 
+        } else if(n.equals("+")) {
+            int b = recurse(st);
+            int a = recurse(st);
+            return a + b;
+        } else if (n.equals("*")) {
+            int b = recurse(st);
+            int a = recurse(st);
+            return a * b;
+        } else if (n.equals("/")){
+            int b = recurse(st);
+            int a = recurse(st);
+            return a / b;
+        } else {
+            return Integer.parseInt(n);
+        }
+    }
+}
