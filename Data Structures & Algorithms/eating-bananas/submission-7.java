bf1734f349fbb0d502cc241f418edd36ffc58890
Tom Thomas <txt450@student.bham.ac.uk>
2026-09-24 19:39:20 +0100

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = 0; 
+        for(int i = 0; i < piles.length; i++) {
+            if(piles[i] > max) {
+                max = piles[i]; 
+            }
+        }
+        int l = 1; 
+        int r = max; 
+        int ans = max;
+        while(l <= r) {
+            int mid = l + (r - l) / 2; 
+            long hrs = noOfHours(piles, mid); 
+            if(hrs <= h) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1; 
+            }
+        }
+        return ans; 
+    }
+    public int noOfHours(int[] piles, int k) {
+        int hrs = 0;
+        for (int pile : piles) {
+            hrs += (int) Math.ceil((double) pile / k);
+        }
+     return hrs;
+    }
+}
